c85c96d49640bebb659f131991b09386af806b4c
Hamza TAS <hamza.tas@getir.com>
2017-09-21 22:04:29 +0300

    folder structure

--- server.py
+++ server.py
@@ -18,27 +18,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def Welcome():
-    return app.send_static_file('index.html')
-
-@app.route('/myapp')
 def WelcomeToMyapp():
     return 'This is for the test sake!'
-
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
