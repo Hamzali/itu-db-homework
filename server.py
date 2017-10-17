efb451a813b3fe15c81aa948432710f92c67d9e8
Hamza TAS <hamza.tas@getir.com>
2017-10-17 16:12:55 +0300

    team file is added

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 import psycopg2
 
 from controllers.students import studentsController
@@ -7,12 +7,15 @@ from controllers.students import studentsController
 app = Flask(__name__)
 appSettings = os.environ['APP_SETTINGS']
 app.config.from_object(appSettings)
+os.
 
 @app.route('/')
 def WelcomeToMyapp():
     return 'This is for the test!'
 
-conn = psycopg2.connect(host="postgres", port="5432", user="marco", password="foobarbaz", database="testdb")
+
+conn = psycopg2.connect(host="104.236.17.86", port="5432",
+                        user="hamzali", password="1954lotr", database="testdb")
 
 # Bind controllers.
 studentsController(app, conn)
@@ -20,4 +23,4 @@ studentsController(app, conn)
 port = int(os.getenv('PORT', '5000'))
 print(appSettings, port)
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
