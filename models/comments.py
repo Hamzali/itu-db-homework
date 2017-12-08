bf997c340b43783511702ea81a9136b8f666ec9b
sahin <oluts@itu.edu.tr>
2017-12-08 21:49:08 +0300

    comments model are added

---
+++ models/comments.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from flask import request
+from constants import MOBIL_ITU_AUTH_URL
+from server import app, auto
+from models.comments import commentsTable
+
+
+@app.route("/comments", methods=['GET', 'POST',
+                                 'PUT', 'DELETE'])
+def comments():
+    """
+    GET request will show comments of student
+    POST request will add comment to student
+    PUT request will update the comment
+    DELETE request will remove the comment
+    """
+    if request.method == 'GET':
+        student_id = request.args.get('sid')
+        data = {"comment_to": str(student_id)}
+        return json.dumps(commentsTable.showCommentsOfStudent(data))
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        
+        return json.dumps(commentsTable.addComment(data))
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        return json.dumps(commentsTable.updateComment(data))
+
+    elif request.method == 'DELETE':
+        data = request.get_json()
+        return(json.dumps(commentsTable.removeComment(data)))
--- server.py
+++ server.py
@@ -30,6 +30,7 @@ def root_path_handler():
 from controllers.students import *
 from controllers.chatGroup import *
 from controllers.courses import *
+from controllers.comments import *
 port = int(os.getenv('PORT', '5000'))
 
 # print(appSettings, port)
