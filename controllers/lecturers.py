4999124b18453f7be9c3e40c2f073313b9988bd3
sahin <oluts@itu.edu.tr>
2017-12-08 22:21:36 +0300

    lecturer controller added

---
+++ controllers/lecturers.py
@@ -0,0 +1,42 @@
+import json
+import requests
+from flask import request
+from constants import MOBIL_ITU_AUTH_URL
+from middlewares import private_route
+from server import app, auto
+from models.lecturers import lecturers
+import re
+
+
+@app.route('/lecturers', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def lecturer():
+    """
+    GET request shows all lecturers
+    POST request creates new lecturer
+    DELETE request deletes a lecturer
+    PUT request updates the lecturer
+    """
+    
+    if request.method == 'GET':
+        return json.dumps(lecturers.listAllLecturers())
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        if not re.match("[^@]+@[^@]+\.[^@]+", data['email']):
+            return "Invalid email address!"
+        return json.dumps(lecturers.addLecturer(data))
+
+    elif request.method == 'DELETE':
+        data = request.get_json()
+        return json.dumps(lecturers.removeLecturer(data))
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        return json.dumps(lecturers.updateLecturer(data))
+
+
+@app.route('/lecturers/<lid>', methods=['GET'])
+def show_a_lecturer(lid):
+
+    if request.method == 'GET':
+        return json.dumps(lecturers.showALecturer(data={'id': lid}))
